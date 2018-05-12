733233ad9def6e5e5182f2177e6d9ee457d47f3e
Ola Thunberg <ola.thunberg@gmail.com>
2018-05-12 10:12:58 +0200

    Checksum calc to prevent duplicates

--- Spotlighter/Program.cs
+++ Spotlighter/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using System.Threading;
 
 namespace Spotlighter
 {
-    class Program
+    public static class Program
     {
         const string SPOTLIGHT_PATH = @"Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
 
@@ -23,6 +25,20 @@ namespace Spotlighter
             if (!Directory.Exists(vertDest))
                 Directory.CreateDirectory(vertDest);
 
+            var destHashes = new List<string>();
+            Console.WriteLine("Computing destination hashes...");
+            foreach (var file in Directory.EnumerateFiles(horizDest))
+            {
+                Console.Write(".");
+                destHashes.Add(CalculateMD5(file));
+            }
+            foreach (var file in Directory.EnumerateFiles(vertDest))
+            {
+                Console.Write(".");
+                destHashes.Add(CalculateMD5(file));
+            }
+
+            Console.WriteLine("");
             Console.WriteLine($"Copying files from '{source}'...");
             Console.WriteLine($"{Directory.GetFiles(source).Length} files found");
             int count = 0;
@@ -33,21 +49,31 @@ namespace Spotlighter
                 {
                     var fInfo = new FileInfo(file);
 
+                    var sourceHash = CalculateMD5(file);
+                    if (destHashes.Contains(sourceHash))
+                        continue;
+
                     // Handle screen based size
                     if (fInfo.Length > 400 * 1024)
                     {
-                        var image = System.Drawing.Image.FromFile(file);
-                        if (image.Width > image.Height)
-                            File.Copy(file, Path.Combine(horizDest, Path.ChangeExtension(Path.GetFileName(file), "jpg")));
-                        else
-                            File.Copy(file, Path.Combine(vertDest, Path.ChangeExtension(Path.GetFileName(file), "jpg")));
+                        using (var image = System.Drawing.Image.FromFile(file))
+                        {
+                            if (image.Width > image.Height)
+                                File.Copy(file, Path.Combine(horizDest, Path.ChangeExtension(Path.GetFileName(file), "jpg")));
+                            else
+                                File.Copy(file, Path.Combine(vertDest, Path.ChangeExtension(Path.GetFileName(file), "jpg")));
+                        }
 
                         count++;
                     }
                 }
-                catch (IOException)
+                catch (IOException ex) when (ex.HResult == -2147024816)
                 {
                     // File already exists
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
                 }
             }
 
@@ -55,6 +81,18 @@ namespace Spotlighter
             Console.WriteLine($"Done, copied {count} new images");
 
             Thread.Sleep(5000);
+        }
+
+        private static string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
         }
     }
 }
